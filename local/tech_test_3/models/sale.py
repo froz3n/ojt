aaf3dffbdb1a470bf6d77c76395c6b70d69e1220
Sonny F A <son.froz.arc@gmail.com>
2022-11-15 16:50:07 +0700

    change var name format to standarized format

--- local/tech_test_3/models/sale.py
+++ local/tech_test_3/models/sale.py
@@ -7,5 +7,5 @@ from datetime import datetime,timedelta
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    sale_warehouse = fields.Many2one("stock.warehouse",string="Warehouse")
+    sale_warehouse_id = fields.Many2one("stock.warehouse",string="Warehouse")
     sale_warehouse_all = fields.Boolean(string="All Warehouse")
