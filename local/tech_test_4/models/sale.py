eff5310b53cf8fb09bec974359919061260b0c11
Sonny F A <son.froz.arc@gmail.com>
2022-11-16 17:44:18 +0700

    tech_test_4 : streamlined code

--- local/tech_test_4/models/sale.py
+++ local/tech_test_4/models/sale.py
@@ -11,40 +11,22 @@ class SaleOrder(models.Model):
     sale_pr_service = fields.Float(string='Service',compute='get_total_per_type')
     sale_pr_stockable = fields.Float(string='Stockable Product',compute='get_total_per_type')
 
-    sale_pr_consumable_exist = fields.Boolean(compute='is_type_exist')
-    sale_pr_service_exist = fields.Boolean(compute='is_type_exist')
-    sale_pr_stockable_exist = fields.Boolean(compute='is_type_exist')
-
-    @api.depends('order_line.price_subtotal')
+    sale_pr_consumable_exist = fields.Boolean(compute='get_total_per_type')
+    sale_pr_service_exist = fields.Boolean(compute='get_total_per_type')
+    sale_pr_stockable_exist = fields.Boolean(compute='get_total_per_type')
+        
+    @api.onchange('order_line')
     def get_total_per_type(self):
         for order in self:
-            sale_pr_consumable = sale_pr_service = sale_pr_stockable = 0.0
+            self.sale_pr_consumable = self.sale_pr_service = self.sale_pr_stockable = 0.0
+            self.sale_pr_consumable_exist = self.sale_pr_service_exist = self.sale_pr_stockable_exist = False
             for line in order.order_line:
                 if line.product_id.type == 'consu':
-                    sale_pr_consumable += line.price_subtotal
-                if line.product_id.type == 'service':
-                    sale_pr_service += line.price_subtotal
-                if line.product_id.type == 'product':
-                    sale_pr_stockable += line.price_subtotal
-            order.update({
-                'sale_pr_consumable' : sale_pr_consumable,
-                'sale_pr_service' : sale_pr_service,
-                'sale_pr_stockable' : sale_pr_stockable,
-            })
-                
-    @api.depends('order_line.order_id')
-    def is_type_exist(self):
-        for order in self:
-            sale_pr_consumable_exist = sale_pr_service_exist = sale_pr_stockable_exist = False
-            for line in order.order_line:
-                if line.product_id.type == 'consu':
-                    sale_pr_consumable_exist = True
-                if line.product_id.type == 'service':
-                    sale_pr_service_exist = True
-                if line.product_id.type == 'product':
-                    sale_pr_stockable_exist = True
-            order.update({
-                'sale_pr_consumable_exist' : sale_pr_consumable_exist,
-                'sale_pr_service_exist' : sale_pr_service_exist,
-                'sale_pr_stockable_exist' : sale_pr_stockable_exist,
-            })
+                    self.sale_pr_consumable += line.price_subtotal
+                    self.sale_pr_consumable_exist = True
+                elif line.product_id.type == 'service':
+                    self.sale_pr_service += line.price_subtotal
+                    self.sale_pr_service_exist = True
+                elif line.product_id.type == 'product':
+                    self.sale_pr_stockable += line.price_subtotal
+                    self.sale_pr_stockable_exist = True
